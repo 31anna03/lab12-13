38825fedd407fc6d6536fe66538ec5c0609d9d6a
annak <annak@DESKTOP-MGDTLFC>
2024-11-14 14:07:34 +0200

    Добавьте файлы проекта.

---
+++ lab12-13.cpp
@@ -0,0 +1,43 @@
+﻿// Створити лінійний односпрямований список із цілих чисел. Видалити зі списку перший парний елемент, який знаходиться на непарній позиції
+//
+
+#include <iostream>
+#include <forward_list>
+#include <windows.h>
+
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    forward_list<int> numberList = { 1, 4, 5, 8, 10, 12 };
+
+    cout << "Список до видалення: ";
+    for (int number : numberList) {
+        cout << number << " ";
+    }
+    cout << endl;
+
+    auto prev = numberList.before_begin(); 
+    auto current = numberList.begin();     
+    int position = 1;
+
+    while (current != numberList.end()) {
+        if (position % 2 != 0 && *current % 2 == 0) {  
+            numberList.erase_after(prev);  
+            break; 
+        }
+        prev = current;
+        ++current;
+        ++position;
+    }
+
+    cout << "Список після видалення: ";
+    for (int number : numberList) {
+        cout << number << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
